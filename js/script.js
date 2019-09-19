0579808c27b189a2f31363035e6fb92f187349ba
Daniel Boham <hello@danielboham.com>
2019-09-19 13:17:07 +0200

    worked on showPage function

--- js/script.js
+++ js/script.js
@@ -2,20 +2,29 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+// Global variables
+const studentList = document.querySelectorAll('li');
+const itemsNumber = 10;
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+
+// showPage function
+const showPage = (list, page) => {
+    let startIndex = (1 * itemsNumber) - itemsNumber;
+    let endIndex = 1 * itemsNumber;
+
+    for (let i = 0; i <= studentList.length; i +=1) {
+       if (i >= startIndex && i < endIndex) {
+           studentList[i].style.display = "block";
+       } else {
+           studentList[i].style.display = "none";
+       }
+    }
+}
+
+showPage();
+
 
 
 
